8422e1a0d576fcd9eabbf2cdbab85a66da2e2601
ahmed <ahmed007eg@gmail.com>
2025-02-18 00:44:50 +0200

    add middleware CORS

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ templates = Jinja2Templates(directory = 'templates')
 @app.get('/index', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
+
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
