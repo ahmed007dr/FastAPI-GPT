9e2e93ebc4874e37791cfde0a5ce9e1c191fff22
ahmed <ahmed007eg@gmail.com>
2025-03-01 21:20:58 +0200

    edit client

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from fastapi import FastAPI
 from baseModel import ChatRequest
 from dotenv import load_dotenv
 import os
-import openai
+from openai import OpenAI
 
 app = FastAPI()
 load_dotenv()
 
-OPENAI_API_KEY = os.environ.get('API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
-openai.api_key = OPENAI_API_KEY
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 
 @app.post("/chat")
 def chat_with_ai(request: ChatRequest):
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": request.message}]
         )
